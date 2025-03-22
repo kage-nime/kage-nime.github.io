c79e5c87db5b76ea567dd9b364025ba5c85e21f1
kage-nime <stokkage951@gmail.com>
2025-03-22 09:31:47 +0700

    Add files via upload

--- kage.js
+++ kage.js
@@ -1,11 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  lockScreen();
+});
+
 function lockScreen() {
   document.getElementById("lock-screen").style.display = "block";
+  document.body.style.overflow = "hidden";
 }
 
 function unlockScreen() {
   const password = document.getElementById("password").value;
-  if (password === "kageno") {
+  if (password === "rahasia") {
     document.getElementById("lock-screen").style.display = "none";
+    document.body.style.overflow = "auto";
   } else {
     alert("Password salah!");
   }
